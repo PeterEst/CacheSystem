38a067f30d538ae7dc3987c69563b1a0dbbdf510
Peter Estephan <peter-estephan@outlook.com>
2023-12-02 14:29:18 +0200

    - CacheInterface 'get' method returns an Optional Object instead of an object

--- src/main/java/Cache/CacheInterface.java
+++ src/main/java/Cache/CacheInterface.java
@@ -1,11 +1,13 @@
 package Cache;
 
+import java.util.Optional;
+
 public interface CacheInterface {
     void put(String key, Object value);
 
     void put(String key, Object value, long timeToLive);
 
-    Object get(String key);
+    Optional<Object> get(String key);
 
     void remove(String key);
 }
